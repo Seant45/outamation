fddb36317244ed8dadda3b6869ed30fb46e2259f
Seant45 <toledosean@gmail.com>
2025-12-25 19:24:13 -0800

    Initial commit

---
+++ python1.py
@@ -0,0 +1,13 @@
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+print("Current date and time:", now)
+
+# Get the current date
+today = datetime.date.today()
+print("Today's date:", today)
+
+# Create a specific date
+my_birthday = datetime.date(2003, 12, 10)
+print("My birthday:", my_birthday)
